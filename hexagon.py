a2fba3e3cf69aa4de247d9a731b7d3c6d4d0bed8
orel <0533orel@gmail.com>
2025-06-25 10:16:46 +0300

    create class Hexagon

---
+++ hexagon.py
@@ -0,0 +1,10 @@
+from math import sqrt
+from calculator import Shape
+
+
+class Hexagon(Shape):
+    def __init__(self, length):
+        self.length = length
+
+    def get_area(self):
+        return (3 * sqrt(3) * (self.length ** 2)) / 2
